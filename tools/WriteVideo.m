7bcca0dab58006479aa72e9a44eb6a9dd5203ae5
hanaecarrie <hanae.carrie@free.fr>
2018-01-22 09:22:24 -0500

    [Add]: script used for DL68_170523_1to4 registration on 01-21-2018

---
+++ tools/WriteVideo.m
@@ -0,0 +1,14 @@
+function []= WriteVideo(SavingPath, Matrix)
+% WRITEVIDEO: this function writes and saves a video file from a matrix
+
+v = VideoWriter(SavingPath);
+
+open(v);
+
+for k = 1 : size(Matrix,3)
+    writeVideo(v, Matrix(:,:,k));
+end
+
+close(v);
+
+end
