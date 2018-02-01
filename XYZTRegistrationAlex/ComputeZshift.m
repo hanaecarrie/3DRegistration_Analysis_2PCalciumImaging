2a21726e6de1b754d26a7de0fea62191ff76c3b4
hanaecarrie <hanae.carrie@free.fr>
2018-02-01 13:11:09 -0500

    [Update]: add interpolation to get the Zshift value

--- XYZTRegistrationAlex/ComputeZshift.m
+++ XYZTRegistrationAlex/ComputeZshift.m
@@ -2,7 +2,7 @@ function[RowShifts,ColumnShifts,ZShifts] = ComputeZshift(...
     tvector, ReferenceVolume, OrderedVolumes, Size)
 
     for t=tvector
-        Correlations=ones(Size(3),2*Size(3)-1)*NaN;
+        Correlations = ones(Size(3),2*Size(3)-1)*NaN;
         for j = 1:Size(3) % considered plane
             for i = 1:Size(3) % ref plane
                 output = dftregistrationAlex(...
@@ -23,19 +23,19 @@ function[RowShifts,ColumnShifts,ZShifts] = ComputeZshift(...
                 num2str(J-Size(3))));
         end
         
-         x = J-5:0.01:J+5;
-         FitOrder = 5;
-         P = polyfit(J-5:J+5, Mean(J-5:J+5),FitOrder);
-         CorrelationFit = polyval(P, x);
-         [~,I] = max(CorrelationFit);
-         figure; plot(x, CorrelationFit); xlim([0, 30]); hold on;
+        x = J-5:0.01:J+5;
+        FitOrder = 5;
+        P = polyfit(J-5:J+5, Mean(J-5:J+5),FitOrder);
+        CorrelationFit = polyval(P, x);
+        [~,I] = max(CorrelationFit);
+        %figure; plot(x-Size(3), CorrelationFit); hold on;
         output = dftregistrationAlex(fft2(mean(ReferenceVolume,3)),...
             fft2(mean(OrderedVolumes(:,:,:,t),3)),100);
         row_shift = output(1);
         column_shift = output(2);
 
-        RowShifts(:,t) = row_shift;
-        ColumnShifts(:,t) = column_shift;
-        ZShifts(t) = -(J-Size(3));
+        RowShifts(:,t) = ones(Size(3),1)*row_shift;
+        ColumnShifts(:,t) = ones(Size(3),1)*column_shift;
+        ZShifts(t) = (I-1)*0.01+J-5-Size(3);
     end
 end
