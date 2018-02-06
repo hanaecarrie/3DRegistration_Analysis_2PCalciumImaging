6bdd33ee994be44d3a25e0a1b95aa0e1c291bc7f
hanaecarrie <hanae.carrie@free.fr>
2018-02-06 11:06:47 -0500

    [Update]: test on fMRI data working with XYZ shifts

--- tools/TestAlexRegistration.m
+++ tools/TestAlexRegistration.m
@@ -2,28 +2,33 @@
 %startup;
 clear all;
 clc;
-cd D:\Analysis_scripts\Dropbox\AndermannLab\users\hanae\code\XYZTRegistrationAlex
+%cd D:\Analysis_scripts\Dropbox\AndermannLab\users\hanae\code\XYZTRegistrationAlex
 
 % load data
-seq = load('E:\hanae_data\test_fMRI_4D_data\filtered_func_data.mat');
-seq = seq.fmri_data;
-seq = seq(:,:,1:15,1:12);
-sizeseq = size(seq); % 64 x 64 x 21 x 180
+full_vol = load('E:\hanae_data\test_fMRI_4D_data\filtered_func_data.mat');
+full_vol = full_vol.fmri_data;
+full_vol = full_vol(:,:,1:15,1:12);
+sizeseq = size(full_vol); % 64 x 64 x 21 x 180
 
 % Choose shifts
 FakeRowShiftsXYZ = zeros(sizeseq(3), sizeseq(4));
+FakeRowShiftsXYZ(7, 10) = 1;
 FakeColumnShiftsXYZ = zeros(sizeseq(3), sizeseq(4));
+FakeColumnShiftsXYZ(8, 5) = 4;
 FakeZShifts = zeros(1, sizeseq(4));
-FakeZShifts(1, 2) = 1;
-FakeZShifts(1, 4) = 2;
-FakeZShifts(1, 6) = -3;
+FakeZShifts(1, 2) = 1.6;
+FakeZShifts(1, 4) = 5.2;
+FakeZShifts(1, 6) = -3.5;
 
 % Apply Shifts to misaligned the sequence of volumes
-MisalignedVolume = ApplyXYZShifts(seq, FakeZShifts, FakeRowShiftsXYZ, ...
+MisalignedVolume = ApplyXYZShifts(full_vol, FakeZShifts, FakeRowShiftsXYZ, ...
     FakeColumnShiftsXYZ);
+% MisalignedVolume = ApplyXYShifts(full_vol, FakeRowShiftsXYZ, ...
+%     FakeColumnShiftsXYZ);
+% MisalignedVolume = ApplyZShifts(MisalignedVolume, FakeZShifts);
 
 % Parameters
-KeepingFactor = 0.9;
+KeepingFactor = 0.95;
 BlurFactor = 1;
 ReferenceVolumeIndex = 1;
 
