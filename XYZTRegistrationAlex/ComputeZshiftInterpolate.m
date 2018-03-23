fa50d4ee827843404c9e6bb7b99b88d1b87f2de1
hanaecarrie <hanae.carrie@free.fr>
2018-03-23 14:21:57 -0400

    [Cosme]: comments function

--- XYZTRegistrationAlex/ComputeZshiftInterpolate.m
+++ XYZTRegistrationAlex/ComputeZshiftInterpolate.m
@@ -1,23 +1,29 @@
 function[RowShifts,ColumnShifts,ZShifts] = ComputeZshiftInterpolate(...
     ReferenceVolumes, Volume, WidthCorr)
-    tic;
-    Size = size(Volume);
-    nbChunck = size(ReferenceVolumes, 4);
-    Chunck = floor(Size(4)/nbChunck);
 
+    tic;
+    % Parameters
+    Size = size(Volume);
+    nbChunck = size(ReferenceVolumes, 4); % nb chuncks of the reference
+    Chunck = floor(Size(4)/nbChunck); % nb frames by chunck of ref
+
+    % Preallocating space for output variables
     RowShifts = zeros(Size(3)-WidthCorr,Size(4));
     ColumnShifts = zeros(Size(3)-WidthCorr,Size(4));
     ZShifts = zeros(Size(3)-WidthCorr,Size(4));
 
-    for t=1:Size(4)
+    for t=1:Size(4) % for each time frame
+        % pick corresponding reference
         reft = ReferenceVolumes(:,:,:,ceil(t/Chunck));
+        % preallocate output vectors
         row_shift = zeros((Size(3)-WidthCorr),1);
         column_shift = zeros((Size(3)-WidthCorr),1);
         z_shift = zeros((Size(3)-WidthCorr),1);
-        for j = WidthCorr+1:Size(3)-WidthCorr % considered plane
+       
+        for j = WidthCorr+1:Size(3)-WidthCorr % j = considered plane
             Corrj = ones(1,2*WidthCorr+1)*NaN;
             
-            for i = j-WidthCorr:j+WidthCorr % ref plane
+            for i = j-WidthCorr:j+WidthCorr % i = reference plane
                 output = dftregistrationAlex(...
                     fft2(reft(:,:,i)),...
                     fft2(Volume(:,:,j,t)),10);
@@ -27,18 +33,22 @@ function[RowShifts,ColumnShifts,ZShifts] = ComputeZshiftInterpolate(...
                     [column_shift row_shift]));
             end
             [~,J] = max(Corrj);
+            % Print shift to the closest plane
             disp(strcat('Zshift volume n°', num2str(t), ' plane n°', ...
                 num2str(j), ':  ', num2str(J-WidthCorr-1)));
+            % Set interpolation vectors and degree regarding matrix size 
             if  (J-5 > 0) & (J+5 <= size(Corrj))
                 idx = 5;
             else
                 idx = (min([length(Corrj)-J, J-1]));
             end
+            % Perform interpolation
             x = J-idx:0.01:J+idx;
             FitOrder = 5;
             P = polyfit(J-idx:J+idx, Corrj(J-idx:J+idx),FitOrder);
             CorrelationFit = polyval(P, x);
-            [~,I] = max(CorrelationFit);
+            [~,I] = max(CorrelationFit); % max of the interpolating curve
+            % gives us the
             output = dftregistrationAlex(fft2(reft(:,:,j)),...
             fft2(Volume(:,:,j,t)),100);
             row_shift(j,1) = output(1);
