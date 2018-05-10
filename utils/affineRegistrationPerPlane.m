2b8c3f4de66486a6052c3c9094e174d6d2797b9f
hanaecarrie <hanae.carrie@free.fr>
2018-05-10 19:09:57 -0400

    [Update]: affine registration per plane

--- utils/affineRegistrationPerPlane.m
+++ utils/affineRegistrationPerPlane.m
@@ -10,7 +10,6 @@ end
 volumeregaffine = [];
 
 for run = runs
-    disp(run);
     if indice == 1
         path = sbxPath(mouse, date, run, 'sbx');
     else
@@ -18,19 +17,24 @@ for run = runs
             '_run', num2str(run), '\',...
             mouse, '_', date, '_', num2str(run-1), '.sbx');
     end
-    out = sbxAlignAffineDFT({path}, 'tbin', 0, 'refsize', ...
-        size(volumereg3, 4), 'refoffset', n);
-    sbxSaveAlignedSBX(path);
-    if indice == 1
-        pathaffine = sbxPath(mouse, date, run, 'sbxreg');
-    else
-        pathaffine = strcat(pathbegin,  'affineplanes\',...
-            date, '_', mouse, '_run', num2str(run), '\',...
-            mouse, '_', date, '_', num2str(run-1), '_reg-0.sbxreg');
+    if exist(strcat(pathbegin, 'affineplanes\', date, '_', mouse, ...
+            '_run', num2str(run), '\', mouse, '_', date, '_', ...
+            num2str(run-1), '_reg-0.sbxreg'))==0
+         disp(run);
+        out = sbxAlignAffineDFT({path}, 'tbin', 0, 'refsize', ...
+            size(volumereg3, 4), 'refoffset', n);
+        sbxSaveAlignedSBX(path);
+%         if indice == 1
+%             pathaffine = sbxPath(mouse, date, run, 'sbxreg');
+%         else
+%             pathaffine = strcat(pathbegin,  'affineplanes\',...
+%                 date, '_', mouse, '_run', num2str(run), '\',...
+%                 mouse, '_', date, '_', num2str(run-1), '_reg-0.sbxreg');
+%         end
+       % plane = sbxReadPMT(pathaffine);
+        %volumeregaffine = cat(4, volumeregaffine, plane);
     end
-    plane = sbxReadPMT(pathaffine);
-    volumeregaffine = cat(4, volumeregaffine, plane);
 end
 
-volumeregaffine = permute(volumeregaffine, [1,2,4,3]);
+%volumeregaffine = permute(volumeregaffine, [1,2,4,3]);
 end
