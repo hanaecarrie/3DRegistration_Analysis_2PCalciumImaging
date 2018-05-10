fcd71d426f88bafb5c4ed9cb6680e0ab419b13fd
hanaecarrie <hanae.carrie@free.fr>
2018-05-10 18:55:36 -0400

    [Update]: 2 arguments added as input: channel and type(sbx, tif or both

--- utils/saveVolumeRegistration.m
+++ utils/saveVolumeRegistration.m
@@ -1,11 +1,14 @@
 function [] = saveVolumeRegistration(savingpath, volume, namefile,...
-    mouse, date, run, nbchuncktiff)
+    mouse, date, run, channel, nbchuncktiff, type)
 
  tic;
  
 [w, h, zp, ts] = size(volume);
 
-if nargin < 7
+if nargin < 9
+    type = 'both';
+end
+if nargin < 8
     nbchuncktiff = 1;
 end
 sizechunck = zp*ts/nbchuncktiff;
@@ -14,12 +17,19 @@ if mod(zp*ts, nbchuncktiff) ~= 0
 end
 
 savingpathv = strcat(savingpath, '\', namefile, '\');
-WriteTiff(savingpathv, mat2gray(double(volume)), strcat(mouse,'_', date,'_', ...
-    num2str(run),'_', namefile), sizechunck);
-sbxpath = sbxPath(mouse, date, run, 'sbx');
-info = sbxInfo(sbxpath);
-sbxWrite(strcat(savingpathv, mouse,'_', date,'_', ...
-    num2str(run),'_', namefile) ,reshape(volume, [w,h,zp*ts]), info);
+
+if strcmp(type,'tif') || strcmp(type,'both') 
+WriteTiffHanae(savingpathv, mat2gray(double(volume)), ...
+    strcat(mouse,'_', date,'_', num2str(run),'_', num2str(channel),...
+    '_', namefile), sizechunck);
+end
+if strcmp(type,'sbx') || strcmp(type,'both')
+    sbxpath = sbxPath(mouse, date, run, 'sbx');
+    info = sbxInfo(sbxpath);
+    sbxWrite(strcat(savingpathv, mouse,'_', date,'_', ...
+        num2str(run),'_', num2str(channel),'_', namefile),...
+        reshape(volume, [w,h,zp*ts]), info);
+end
 
 tEnd = toc;
 fprintf('Elapsed time is %d minutes and %f seconds\n.', ...
