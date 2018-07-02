bc35b38e5186af298fba30ecf09d1bced3aafafb
hanaecarrie <hanae.carrie@free.fr>
2018-07-02 08:29:02 -0400

    [Fix]: script plot shifts XYZ independant now

--- Registration3D/Plots/Shifts.m
+++ Registration3D/Plots/Shifts.m
@@ -1,8 +1,11 @@
-clear all;
-% Column_DL102 = cat(3, Column_DL102, ColumnShiftsXY1 + ColumnShiftsXY2 + cat(1, ColumnShiftsZ, zeros(3,930)) + ColumnShiftsW');
-% Row_DL102 = cat(3, Row_DL102, RowShiftsXY1 + RowShiftsXY2 + cat(1, RowShiftsZ, zeros(3,930)) + RowShiftsW');
-% Z_DL102 = cat(3, Z_DL102, ZShifts);
+%% Script Shifts figures DL89 (part 1) and DL102 (part 2)
+% On Magatron
 
+% PART1: DL89 Z shifts
+
+clear all;
+
+%% Load shifts
 
 path = 'E:\hanae_data\Dura\AnalysisDura\';
 Column_DL89before = load(strcat(path, 'Column_DL89before'));
@@ -17,9 +20,8 @@ Rall = reshape(Row_DL89before, [30, 6*930]);
 Zall = reshape(Z_DL89before, [30, 6*930]);
 Row_DL89before = reshape(Row_DL89before, [30, 930*6]);
 
-imagesc(Row_DL89before(:,1:930));
+%% preprocess Z shifts
 
-%%
 Z = Zall;
 for i = 1:30
 Z(i,:) = Z(i,:) +i;
@@ -57,26 +59,15 @@ n = 30*20; % average every n values
 a = reshape(running,[],1); % arbitrary data
 avgrun = arrayfun(@(i) mean(a(i:i+n-1)),1:n:length(a)-n+1)';
 
-%%
-
-n = 20;
-x =(1:6)*(930/n)*(30/15.5)/60;
-hold on;
-h = plot((avgrun')/3-2.6, 'r');
-plot(ZDL89mean(:,1:end-3)');
-hold off;
-legend(h, 'running state', 'Location', 'Best');
-ylim([-2.6 27]);
-xlabel('time (min)');
-ylabel('plane relative position (no unit)');
-title('Plane position DL102 across time');
 
 %% heatmap Z
+
 ZDL89meanshifts = ZDL89mean';
 for i = 1:24
     ZDL89meanshifts(i,:) = ZDL89meanshifts(i,:)-3-i;
 end
 
+figure;
 imagesc(flipud(ZDL89meanshifts(1:21,:))); colorbar;
 % yticks(1:24);
 % yticklabels({'24', '23','22','21','20', '19','18', ...
@@ -89,29 +80,8 @@ ylabel('plane number');
 title('Z Shifts per plane across time DL89');
 %caxis([-1.3 2.4]);
 
-%%
 
-running = sbxSpeed('DL89', '171122', 1);
-for i = 2:6
-running = cat(2, running, sbxSpeed('DL89', '171122', i));
-end
-n = 30*20; % average every n values
-a = reshape(running,[],1); % arbitrary data
-avgrun = arrayfun(@(i) mean(a(i:i+n-1)),1:n:length(a)-n+1)';
-figure;
-meanZ = mean(Zall,1);
-plot(smooth(mean(ZDL89meanshifts, 1)));
-%plot(smooth(smooth(meanZ)));
-figure;
-ZDL89meanall = ZDL89mean';
-for i = 1:24
-    ZDL89meanall(i,:) = ZDL89meanall(i,:) - (i+3); 
-end 
-ZDL89meanall = mean((ZDL89meanall), 1);
-%plot((diff(ZDL89meanall)));
-hold on; plot(avgrun/50-0.3);
-
-%%
+%% Mean Zshift VS Running state
 
 running = sbxSpeed('DL89', '171122', 1);
 for i = 2:6
@@ -125,11 +95,17 @@ meanZDL89align = mean(ZDL89align, 1)';
 n = 30; % average every n values
 b = reshape(meanZDL89align,[],1); % arbitrary data
 avgmeanZDL89 = arrayfun(@(i) mean(b(i:i+n-1)),1:n:length(b)-n+1)';
+figure;
 plot((avgmeanZDL89-mean(avgmeanZDL89))'); hold on; plot(avgrun/10-1);
+title('mean Z shifts across time VS running state - DL89');
 
-for i = 2:6
-    corrZDL89(i) = corr(meanZDL89align(930*(i-1)+1:930*i)', ...
-        avgrun(930*(i-1)+1:930*i));
+%% Correlation per run
+
+n = 30;
+avgrun = arrayfun(@(i) mean(a(i:i+n-1)),1:n:length(a)-n+1);
+for i = 1:6
+    corrZDL89(i) = corr(meanZDL89align(930*(i-1)+1:930*i), ...
+        avgrun(930*(i-1)+1:930*i)');
 end
 figure;
 bar(corrZDL89);
@@ -138,68 +114,13 @@ ylabel('correlation value');
 title('correlation between mean Z shift and running state for each run DL89');
 
 
-%%
-nx = 45;
-corrDL89 = zeros(1, floor(279/nx));
-diffZDL89 = diff(ZDL89meanall);
-for i = 1:floor(279/nx)
-    corridx = corr(ZDL89meanall((i-1)*nx+1:i*nx)', avgrun((i-1)*nx+1:i*nx));
-    corrDL89(i) = corridx;
-end
-xbis = (1:279/nx)*20*nx*(30/15.5)/60;
-%plot(xbis, corrDL89);
-bar(xbis, (corrDL89));
-%%
-plot(smooth(smooth(meanZ)));
-
-running = sbxSpeed('DL89', '171122', 1);
-for i = 2:6
-running = cat(2, running, sbxSpeed('DL89', '171122', i));
-end
-n = 30*20; % average every n values
-a = reshape(running,[],1); % arbitrary data
-avgrun = arrayfun(@(i) mean(a(i:i+n-1)),1:n:length(a)-n+1)';
-
-ZDL89meanall = ZDL89mean';
-for i = 1:24
-    ZDL89meanall(i,:) = ZDL89meanall(i,:) - (i+3); 
-end 
-
-%%
-
-% running
-running = sbxSpeed('DL89', '171122', 1);
-for i = 2:6
-running = cat(2, running, sbxSpeed('DL89', '171122', i));
-end
-n = 30; % average every n values
-a = reshape(running,[],1); % arbitrary data
-avgrun = arrayfun(@(i) mean(a(i:i+n-1)),1:n:length(a)-n+1)';
-
-meanZ = mean(Zall);
-figure;
-meanZ = mean((ZDL89meanall), 1);
-plot(smooth(meanZ));
-hold on; plot(avgrun/50-0.3);
-
-n = 930;
-corrZrun = [];
-for i = 1:6*930/n
-corrZrun = cat(1, corrZrun, corr((meanZ((i-1)*n+1:n*i))', avgrun((i-1)*n+1:n*i)));
-end
-figure; bar((corrZrun));
-% 
-% n = 180; corrZrun = [];
-% for i = 1:6*930/n
-% corrZrun = cat(1, corrZrun, corr((meanZ((i-1)*n+1:n*i))', avgrun((i-1)*n+1:n*i)));
-% end
-% figure; bar(abs(corrZrun))
-
-
 %% Shifts DL102
 
 clear all;
 
+%% COLUMN Shifts
+
+% initialize path and matrix
 pathbegin = 'E:\hanae_data\Dura\regdata\DL102\DL102_180514_';
 ColumnXY1DL102 = zeros(30, 930, 6);
 ColumnXY2DL102 = zeros(30, 930, 6);
@@ -208,8 +129,8 @@ ColumnZDL102 = zeros(30, 930, 6);
 RowDL102 = zeros(30, 930, 6);
 ZDL102 = zeros(30, 930, 6);
 
+% read shift values
 for runidx = 1:6
-    disp(runidx)
     pathidxC = strcat(pathbegin, num2str(runidx),'\DL102_180514_',...
         num2str(runidx), '_0\ShiftsColumn\ColumnShifts');
     aux1 = load(strcat(pathidxC, 'XY1'));
@@ -227,34 +148,36 @@ for runidx = 1:6
     ColumnWDL102(:,:,runidx) = aux';
 end
 
-
-%%
-
+% reshape and combine shifts
 ColumnWDL102 = reshape(ColumnWDL102, [30, 930*6]);
 ColumnXY1DL102 = reshape(ColumnXY1DL102, [30, 930*6]);
 ColumnXY2DL102 = reshape(ColumnXY2DL102, [30, 930*6]);
 ColumnZDL102 = reshape(ColumnZDL102, [30, 930*6]);
 ColumnDL102 = ColumnWDL102 + ColumnXY1DL102 + ColumnXY2DL102 + ColumnZDL102;
 
-%%
-figure; imagesc(ColumnWDL102); colorbar; caxis([-10 10]); title('ColumnW DL102');
-figure; imagesc(ColumnXY1DL102); colorbar; caxis([-20 20]); title('ColumnXY1 DL102');
-figure; imagesc(ColumnZDL102); colorbar; caxis([-1 1]);  title('ColumnZ DL102');
-figure; imagesc(ColumnXY2DL102); colorbar; caxis([-1 1]);  title('ColumnXY2 DL102');
-figure; imagesc(ColumnDL102); colorbar;  title('ColumnALL DL102');
+% figures
+figure; imagesc(ColumnWDL102); colorbar; caxis([-10 10]);
+title('ColumnW DL102');
+figure; imagesc(ColumnXY1DL102); colorbar; caxis([-20 20]);
+title('ColumnXY1 DL102');
+figure; imagesc(ColumnZDL102); colorbar; caxis([-1 1]);
+title('ColumnZ DL102');
+figure; imagesc(ColumnXY2DL102); colorbar; caxis([-1 1]);
+title('ColumnXY2 DL102');
+figure; imagesc(ColumnDL102); colorbar;
+title('ColumnALL DL102');
 
+%% ROWS shifts
 
-%%
-
+% initialize matrix and get path
 pathbegin = 'E:\hanae_data\Dura\regdata\DL102\DL102_180514_';
 RowXY1DL102 = zeros(30, 930, 6);
 RowXY2DL102 = zeros(30, 930, 6);
 RowWDL102 = zeros(30, 930, 6);
 RowZDL102 = zeros(30, 930, 6);
 
-
+% read shift values
 for runidx = 1:6
-    disp(runidx)
     pathidxC = strcat(pathbegin, num2str(runidx),'\DL102_180514_',...
         num2str(runidx), '_0\ShiftsRow\RowShifts');
     aux1 = load(strcat(pathidxC, 'XY1'));
@@ -272,30 +195,32 @@ for runidx = 1:6
     RowWDL102(:,:,runidx) = aux';
 end
 
-
-%%
-
+% reshape and combine rows
 RowWDL102 = reshape(RowWDL102, [30, 930*6]);
 RowXY1DL102 = reshape(RowXY1DL102, [30, 930*6]);
 RowXY2DL102 = reshape(RowXY2DL102, [30, 930*6]);
 RowZDL102 = reshape(RowZDL102, [30, 930*6]);
 RowDL102 = RowWDL102 + RowXY1DL102 + RowXY2DL102 + RowZDL102;
 
-%%
-figure; imagesc(RowWDL102); colorbar; caxis([-10 10]); title('RowW DL102');
-figure; imagesc(RowXY1DL102); colorbar; caxis([-20 20]); title('RowXY1 DL102');
-figure; imagesc(RowZDL102); colorbar; caxis([-1 1]);  title('RowZ DL102');
-figure; imagesc(RowXY2DL102); colorbar; caxis([-1 1]);  title('RowXY2 DL102');
-figure; imagesc(RowDL102); colorbar;  title('RowALL DL102');
+% figures
+figure; imagesc(RowWDL102); colorbar; caxis([-10 10]);
+title('RowW DL102');
+figure; imagesc(RowXY1DL102); colorbar; caxis([-20 20]);
+title('RowXY1 DL102');
+figure; imagesc(RowZDL102); colorbar; caxis([-1 1]); 
+title('RowZ DL102');
+figure; imagesc(RowXY2DL102); colorbar; caxis([-1 1]); 
+title('RowXY2 DL102');
+figure; imagesc(RowDL102); colorbar; 
+title('RowALL DL102');
 
 
-%%
+%% Z shifts
 
 pathbegin = 'E:\hanae_data\Dura\regdata\DL102\DL102_180514_';
 ZDL102 = zeros(30, 930, 6);
 
 for runidx = 1:6
-    disp(runidx)
     pathidxZ = strcat(pathbegin, num2str(runidx),'\DL102_180514_',...
         num2str(runidx), '_0\ShiftsZ\ZShifts');
     aux = load(pathidxZ);
@@ -304,15 +229,11 @@ for runidx = 1:6
     ZDL102(:,:,runidx) = aux;
 end
 
-
-%%
-
 ZDL102 = reshape(ZDL102, [30, 930*6]);
+figure; imagesc(ZDL102(4:27,:)); colorbar; caxis([-1.5 1.5]);
+title('Z DL102');
 
-
-%%
-figure; imagesc(ZDL102(4:27,:)); colorbar; caxis([-1.5 1.5]); title('Z DL102');
-%%
+% reshape and smooth shifts
 ZDL102planes = ZDL102;
 for i = 1:30
     ZDL102planes(i,:) = ZDL102planes(i,:) + i; 
@@ -322,13 +243,12 @@ ZDL102planessmooth = ZDL102planes;
 for i = 1:30
     ZDL102planessmooth(i,:) = smooth(smooth(ZDL102planes(i,:))); 
 end
-%%
 ZDL102align = ZDL102planessmooth;
 ZmeanbeforeCSD = mean(ZDL102align(:, 1:1860), 2);
 for i = 1:30
     ZDL102align(i,:) = ZDL102align(i,:) - ZmeanbeforeCSD(i) + i; 
 end 
 
-
-%%
+% figure
 figure; plot(ZDL102align(4:27,:)');
+title('Z planes position across time - DL102');
